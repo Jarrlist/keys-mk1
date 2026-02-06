1acfdfe2b347f71180a70024e6fdf0e5a5c1a871
Jarre <jarrlist@gmail.com>
2026-02-06 20:38:42 +0100

    pcb

--- ergogen/footprints/jstph.js
+++ ergogen/footprints/jstph.js
@@ -13,6 +13,16 @@ module.exports = {
                 ${p.at}
                 (fp_text reference "${p.ref}" (at 0 0) (layer ${side}.SilkS) ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15)) ${mirror}))
                 (fp_text value "" (at 0 0) (layer ${side}.Fab) hide (effects (font (size 1 1) (thickness 0.15)) ${mirror}))
+                
+                ${''/* Silkscreen Outline */}
+                (fp_line (start -2.95 -1.35) (end -2.95 6.25) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start -2.95 6.25) (end 2.95 6.25) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start 2.95 6.25) (end 2.95 -1.35) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start 2.95 -1.35) (end -2.95 -1.35) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start -1.5 6.25) (end -1.5 1.25) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start 1.5 6.25) (end 1.5 1.25) (layer ${side}.SilkS) (width 0.15))
+                (fp_line (start -1.5 1.25) (end 1.5 1.25) (layer ${side}.SilkS) (width 0.15))
+
                 (pad 1 thru_hole rect (at -1 0 ${p.rot}) (size 1.2 1.7) (drill 0.8) (layers *.Cu *.Mask) ${p.pos.str})
                 (pad 2 thru_hole oval (at 1 0 ${p.rot}) (size 1.2 1.7) (drill 0.8) (layers *.Cu *.Mask) ${p.neg.str})
             )
