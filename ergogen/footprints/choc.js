18081c7c5e84d00b2a9cf59975a2acc9e1f98bf1
Jarre <jarrlist@gmail.com>
2026-02-06 17:56:40 +0100

    switches

--- ergogen/footprints/choc.js
+++ ergogen/footprints/choc.js
@@ -4,11 +4,12 @@ module.exports = {
     keycaps: false,
     reverse: false,
     hotswap: true,
+    side: 'F',
     from: undefined,
     to: undefined
   },
   body: p => {
-    const standard = `
+    const common = `
       (module PG1350 (layer F.Cu) (tedit 5D5C6E6C)
       ${p.at /* parametric position */}
 
@@ -17,28 +18,39 @@ module.exports = {
       (fp_text value PG1350 (at 0 -8) (layer F.Fab) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
       ${''/* corner marks */}
-      (fp_line (start -7 -6) (end -7 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -7 -7) (end -6 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -6 7) (end -7 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start -7 7) (end -7 6) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 6) (end 7 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 7) (end 6 7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 6 -7) (end 7 -7) (layer F.SilkS) (width 0.15))
-      (fp_line (start 7 -7) (end 7 -6) (layer F.SilkS) (width 0.15))
+      (fp_line (start -7 -6) (end -7 -7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -7 7) (end -6 7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -6 -7) (end -7 -7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start -7 7) (end -7 6) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 7 6) (end 7 7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 7 -7) (end 6 -7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 6 7) (end 7 7) (layer Dwgs.User) (width 0.15))
+      (fp_line (start 7 -7) (end 7 -6) (layer Dwgs.User) (width 0.15))      
       
       ${''/* central hole */}
       (pad "" np_thru_hole circle (at 0 0) (size 3.429 3.429) (drill 3.429) (layers *.Cu *.Mask))
       
-      ${''/* side holes */}
+      ${''/* side holes (stabilizers) */}
       (pad "" np_thru_hole circle (at -5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
       (pad "" np_thru_hole circle (at 5.5 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
 
-      ${''/* pins */}
-      (pad 1 thru_hole circle (at 0 5.9) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.from.str})
-      (pad 2 thru_hole circle (at 5 3.8) (size 2.032 2.032) (drill 1.27) (layers *.Cu *.Mask) ${p.to.str})
+      ${''/* hotswap holes */}
+      (pad "" np_thru_hole circle (at 0 -5.95) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+      (pad "" np_thru_hole circle (at 5 -3.75) (size 3 3) (drill 3) (layers *.Cu *.Mask))
     `
+
+    // Determine pad side (opposite of placement side for hotswap)
+    // If placed on Top (F), pads on Bottom (B).
+    // If placed on Bottom (B), pads on Top (F).
+    // Note: This logic assumes the switch is placed on `p.side` and the socket is on the other side.
+    const pad_side = p.side === 'F' ? 'B' : 'F';
+    const layers = `"${pad_side}.Cu" "${pad_side}.Paste" "${pad_side}.Mask"`;
+
     return `
-      ${standard}
+      ${common}
+      ${''/* pads */}
+      (pad "1" smd rect (at -3.275 -5.95 ${p.rot + 180}) (size 2.6 2.6) (layers ${layers}) ${p.from.str})
+      (pad "2" smd rect (at 8.275 -3.75 ${p.rot + 180}) (size 2.6 2.6) (layers ${layers}) ${p.to.str})
       )
     `
   }
--- ergogen/footprints/slider.js
+++ ergogen/footprints/slider.js
@@ -6,15 +6,41 @@ module.exports = {
         to: { type: 'net', value: 'to' }
     },
     body: p => {
-        const side = p.side === 'B' ? 'B' : 'F';
-        const mirror = p.side === 'B' ? '(justify mirror)' : '';
+        const side = p.side;
+        const opp = side === 'B' ? 'F' : 'B';
+        const mirror = side === 'B' ? '(justify mirror)' : '';
+        const rot = p.rot || 0;
+
+        // Pads on opposite side
+        const layers = `"${opp}.Cu" "${opp}.Paste" "${opp}.Mask"`;
+
         return `
-            (module SPDT_C128955 (layer ${side}.Cu) (tedit 6060E584)
+            (module "E73:SPDT_C128955" (layer ${side}.Cu) (tedit 6060E584)
                 ${p.at}
-                (fp_text reference "${p.ref}" (at 0 0) (layer ${side}.SilkS) ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15)) ${mirror}))
-                (pad 1 thru_hole rect (at -2.54 0 ${p.rot}) (size 1.5 2.5) (drill 0.9) (layers *.Cu *.Mask) ${p.from.str})
-                (pad 2 thru_hole oval (at 0 0 ${p.rot}) (size 1.5 2.5) (drill 0.9) (layers *.Cu *.Mask) ${p.to.str})
-                (pad 3 thru_hole oval (at 2.54 0 ${p.rot}) (size 1.5 2.5) (drill 0.9) (layers *.Cu *.Mask))
+                (fp_text reference "${p.ref}" (at 0 0) (layer ${side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15)) ${mirror}))
+                (fp_text value "" (at 0 0) (layer ${side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15)) ${mirror}))
+
+                ${''/* Silkscreen Lines (simplified based on bounding box) */}
+                (fp_line (start -3.3 -1.35) (end -3.3 1.5) (stroke (width 0.15) (type solid)) (layer ${side}.SilkS))
+                (fp_line (start -3.3 1.5) (end 3.3 1.5) (stroke (width 0.15) (type solid)) (layer ${side}.SilkS))
+                (fp_line (start 0 -1.35) (end -3.3 -1.35) (stroke (width 0.15) (type solid)) (layer ${side}.SilkS))
+                (fp_line (start 0 -1.35) (end 3.3 -1.35) (stroke (width 0.15) (type solid)) (layer ${side}.SilkS))
+                (fp_line (start 3.3 1.5) (end 3.3 -1.35) (stroke (width 0.15) (type solid)) (layer ${side}.SilkS))
+
+                ${''/* Mounting Pads (Mechanical) */}
+                (pad "" smd rect (at -3.7 -1.1 ${rot + 270}) (size 0.9 0.9) (layers ${layers}))
+                (pad "" smd rect (at -3.7 1.1 ${rot + 270}) (size 0.9 0.9) (layers ${layers}))
+                (pad "" smd rect (at 3.7 -1.1 ${rot + 270}) (size 0.9 0.9) (layers ${layers}))
+                (pad "" smd rect (at 3.7 1.1 ${rot + 270}) (size 0.9 0.9) (layers ${layers}))
+
+                ${''/* Alignment Holes */}
+                (pad "" np_thru_hole circle (at -1.5 0 ${rot}) (size 1 1) (drill 0.9) (layers *.Cu *.Mask))
+                (pad "" np_thru_hole circle (at 1.5 0 ${rot}) (size 1 1) (drill 0.9) (layers *.Cu *.Mask))
+
+                ${''/* Electrical Pins */}
+                (pad "1" smd rect (at -2.25 2.075 ${rot + 270}) (size 0.9 1.25) (layers ${layers}) ${p.from.str})
+                (pad "2" smd rect (at 0.75 2.075 ${rot + 270}) (size 0.9 1.25) (layers ${layers}) ${p.to.str})
+                (pad "3" smd rect (at 2.25 2.075 ${rot + 270}) (size 0.9 1.25) (layers ${layers}))
             )
         `
     }
